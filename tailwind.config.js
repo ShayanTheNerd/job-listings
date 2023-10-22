601386872bdb67e03c92f724b5d26250b5bf19f6
Shayan Zamani <sh.zamani384@gmail.com>
2023-10-22 13:18:17 +0330

    feat: add essential files and configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 export default {
 	content: ['./index.html', './src/**/*.vue'],
 	future: { hoverOnlyWhenSupported: true },
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['League Spartan', 'Arial', 'Helvetica', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
 };
