573e50bc8f2fe3dc53b7bcbe2acba11ca7686ceb
Shayan Zamani <sh.zamani384@gmail.com>
2023-11-09 23:30:43 +0330

    fix: fix layout shifts caused by visible scrollbars

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -8,7 +8,8 @@
 </script>
 
 <template>
-	<main class="bg-lilacWhite pb-6 pt-44 sm:pt-32 md:pt-28 lg:pt-24">
+	<main
+		class="scrollbar-stable-both h-px overflow-y-auto scroll-smooth bg-lilacWhite pb-6 pt-44 sm:pt-32 md:pt-28 lg:pt-24">
 		<nav v-if="filteredJobs" class="mx-auto w-88% max-w-screen-lg">
 			<ul class="space-y-12 lg:space-y-6">
 				<JobCard v-for="job in filteredJobs" :key="job.id" :job="job" />
@@ -24,5 +25,13 @@
 <style>
 	main {
 		min-height: calc(100vh - theme('spacing.36'));
+	}
+
+	@media (min-width: theme('screens.lg')) {
+		@supports (scrollbar-gutter: stable both-edges) {
+			nav {
+				width: 90.7% !important;
+			}
+		}
 	}
 </style>
