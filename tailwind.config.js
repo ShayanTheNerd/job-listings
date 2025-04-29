ad597ff9d058d558e9738a367a0864773772397d
Shayan Zamani <dev.shayan.zamani@gmail.com>
2025-04-29 23:37:57 +0330

    Verified for Netlify 364974

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-
 import defaultTheme from 'tailwindcss/defaultTheme';
 
+/** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./index.html', './src/**/*.vue'],
 	future: { hoverOnlyWhenSupported: true },
